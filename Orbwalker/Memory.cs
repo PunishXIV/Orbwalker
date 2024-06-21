0818865bff22094039bbf61b4835bc765db347a5
Limiana <5073202+Limiana@users.noreply.github.com>
2024-06-21 20:53:28 +0300

    commit all changes

--- Orbwalker/Memory.cs
+++ Orbwalker/Memory.cs
@@ -55,7 +55,7 @@ namespace Orbwalker
                 try
                 {
                     InternalLog.Verbose($"{type}, {acId}, {target}");
-                    if (P.DelayedAction == null && ((type == ActionType.Spell && Util.IsActionCastable(acId)) || type == ActionType.Mount) && Util.GCD == 0 && AgentMap.Instance()->IsPlayerMoving != 0 && !am->ActionQueued && !Util.CheckTpRetMnt(acId, type))
+                    if (P.DelayedAction == null && ((type == ActionType.Action && Util.IsActionCastable(acId)) || type == ActionType.Mount) && Util.GCD == 0 && AgentMap.Instance()->IsPlayerMoving != 0 && !am->ActionQueued && !Util.CheckTpRetMnt(acId, type))
                     {
                         P.DelayedAction = new(acId, type, 0, target, a5, a6, a7, a8);
                         return false;
--- Orbwalker/UI.cs
+++ Orbwalker/UI.cs
@@ -307,7 +307,7 @@ namespace Orbwalker
             if (Svc.Targets.Target != null)
             {
                 var addInfo = stackalloc uint[1];
-                ImGuiEx.Text($"{ActionManager.Instance()->GetActionStatus(ActionType.Spell, 16541, Svc.Targets.Target.Struct()->GetObjectID(), outOptExtraInfo: addInfo)} / {*addInfo}");
+                ImGuiEx.Text($"{ActionManager.Instance()->GetActionStatus(ActionType.Action, 16541, Svc.Targets.Target.Struct()->GetObjectID(), outOptExtraInfo: addInfo)} / {*addInfo}");
             }
             ImGuiEx.Text($"GCD: {Util.GCD}\nRCorGRD:{Util.GetRCorGDC()}");
         }
