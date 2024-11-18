4d171bc7c79a492bf9159d705dafa7bc97f0c174
Limiana <5073202+Limiana@users.noreply.github.com>
2024-11-18 09:41:30 +0300

    7.1

--- Orbwalker/Memory.cs
+++ Orbwalker/Memory.cs
@@ -112,7 +112,7 @@ namespace Orbwalker
 
 
         delegate byte InputData_IsInputIDKeyHeldDelegate(nint a1, int key);
-        [Signature("E8 ?? ?? ?? ?? 84 DB 0F B6 D0", DetourName = nameof(InputData_IsInputIDKeyHeldDetour), Fallibility = Fallibility.Infallible)]
+        [Signature("E8 ?? ?? ?? ?? 84 DB 44 0F B6 C0", DetourName = nameof(InputData_IsInputIDKeyHeldDetour), Fallibility = Fallibility.Infallible)]
         Hook<InputData_IsInputIDKeyHeldDelegate> InputData_IsInputIDKeyHeldHook;
         byte InputData_IsInputIDKeyHeldDetour(nint a1, int key)
         {
